0c69c909273b8f50f23af6011360c8c3b2da5212
Arnom Maciel <arnom.maciel@g.globo>
2021-06-10 17:54:24 -0300

    add custom middleware

---
+++ tracing/opentelemetry/custommid.go
@@ -0,0 +1,42 @@
+package opentelemetry
+
+import (
+	"net/http"
+
+	otelcontrib "go.opentelemetry.io/contrib"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/semconv"
+	oteltrace "go.opentelemetry.io/otel/trace"
+)
+
+func OtelCustomMiddleware(service string, pattern string, h http.Handler) http.Handler {
+	cfg := config{}
+	if cfg.TracerProvider == nil {
+		cfg.TracerProvider = otel.GetTracerProvider()
+	}
+	tracer := cfg.TracerProvider.Tracer(
+		tracerName,
+		oteltrace.WithInstrumentationVersion(otelcontrib.SemVersion()),
+	)
+	if cfg.Propagators == nil {
+		cfg.Propagators = otel.GetTextMapPropagator()
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := cfg.Propagators.Extract(r.Context(), propagation.HeaderCarrier(r.Header))
+		spanName := ""
+		spanName = r.URL.Path
+		opts := []oteltrace.SpanOption{
+			oteltrace.WithAttributes(semconv.NetAttributesFromHTTPRequest("tcp", r)...),
+			oteltrace.WithAttributes(semconv.EndUserAttributesFromHTTPRequest(r)...),
+			oteltrace.WithAttributes(semconv.HTTPServerAttributesFromHTTPRequest("GLIVE-SERVICE", spanName, r)...),
+			oteltrace.WithSpanKind(oteltrace.SpanKindServer),
+		}
+		ctx, span := tracer.Start(ctx, spanName, opts...)
+		defer span.End()
+
+		cfg.Propagators.Inject(ctx, propagation.HeaderCarrier(r.Header))
+		h.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
